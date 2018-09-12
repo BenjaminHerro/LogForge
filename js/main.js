4e179ac16f08b106b0a50497de05389f6a6adb12
Benjamin Herro <benjamincsherro@hotmail.com>
2018-09-12 17:39:41 +1000

    Added some stuff

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 var newTableLine;
 var newRowData;
 var totalHours = 0;
-var time = new Date('07/20/2018 6:30PM');
-var caringbah = {lat: -34.0452, lng: 151.1218}
+// var time = new Date('07/20/2018 6:30PM');
+var origin = document.querySelector('input#initialO');
+var odometer = 71564;
+var service = new google.maps.DistanceMatrixService();
+let startDate = document.querySelector('input#startD');
+let startTime = document.querySelector('input#startT');
+var destination;
 
-const addRow = (previousDate) => {
+// const getSydSubs = (nsw,syd) => {
+// 	let sydney_suburbs = [];
+// 	for (i=0;i<nsw.length;i++) {
+// 		if (syd.includes(nsw[i][1])) {
+// 			sydney_suburbs.push(nsw[i]);
+// 		}
+// 	}
+// 	return sydney_suburbs;
+// };
 
+// sydney_suburbs = getSydSubs(nsw_suburbs,syd_suburbs)
+
+const choice = (choices) => {
+	let i = Math.floor(Math.random()*choices.length);
+	return choices[i];
 };
+
+
 
 const calcTimeDifference = (startTime,endTime) => {
 	let startTimeHours = startTime.getHours();
@@ -20,28 +40,61 @@ const calcTimeDifference = (startTime,endTime) => {
 };
 
 class RowObjectT1 {
-	constructor(start,finish,from,to,od_before,od_after,distance) {
-		let duration = calcTimeDifference(start,finish);
+	constructor(start,from,to,od_before) {
 		this.date = start.toLocaleDateString();
 		this.start = start.toLocaleTimeString();
 		this.finish = finish.toLocaleTimeString();
 		this.duration = duration[0].toString() + 'h ' +  duration[1].toString() + 'm';
+		this.google_maps(from,to);
 		this.from = from;
 		this.to = to;
 		this.od_before = od_before;
 		this.od_after = od_after;
 	}
 
-};
-
-class RowObjectT2 {
-	constructor(road,weather,) {
-
+	google_maps(origin,destination) {
+		service.getDistanceMatrix(
+		  {
+		    origins: [this.from],
+		    destinations: [this.to],
+		    travelMode: 'DRIVING',
+		  }, this.callback);
 	}
 
+	callback(response, status) {
+		console.log(status)
+		  if (status == 'OK') {
+		    var origins = response.originAddresses;
+		    var destinations = response.destinationAddresses;
+
+		    for (var i = 0; i < origins.length; i++) {
+		      var results = response.rows[i].elements;
+		      for (var j = 0; j < results.length; j++) {
+		        var element = results[j];
+		        this.distance = element.distance.text;
+		        this.duration = element.duration.text;
+		        // var from = origins[i];
+		        // var to = destinations[j];
+	     		}
+	   		}
+	  	}
+	}
 };
 
+// class RowObjectT2 {
+// 	constructor(road,weather,) {
+
+// 	}
+
+// };
+
 while (totalHours <= 120) {
+	// service.getDistanceMatrix(
+	// 	  {
+	// 	    origins: [this.from],
+	// 	    destinations: [this.to],
+	// 	    travelMode: 'DRIVING',
+	// 	  }, callback);
 	newTableRow = document.createElement('tr');
 	newRowData = document.createElement('td');
 
